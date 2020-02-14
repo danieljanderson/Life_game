d5e096aec22baaeaae9b5acc0fd0c615f230bc29
danieljanderson <danieljanderson0616@gmail.com>
2020-02-14 15:27:36 -0500

    created methods on the person class

--- src/Person.js
+++ src/Person.js
@@ -5,7 +5,7 @@ class Person {
     gender,
     money,
     charisma,
-    determination,
+    feeling,
     intelligence,
     hobbies,
     location,
@@ -17,32 +17,77 @@ class Person {
     this._gender = gender;
     this._money = money;
     this._charisma = charisma;
-    this._determination = determination;
+    this._feeling = feeling;
     this._intelligence = intelligence;
     this._hobbies = hobbies;
     this._location = location;
     this._employmentHistory = employmentHistory;
     this._numberOfApplications = numberOfApplications;
   }
+  // getters and setters
+  // get and set for names
+  get name() {
+    return this._name;
+  }
+  set name(newName) {
+    this._name = newName;
+  }
+  //get and set for money
+  get money() {
+    return this._money;
+  }
   set money(newAmount) {
     this._money = newAmount + this._money;
+  }
+  // get and set for charisma
+  get charisma() {
+    return this._charisma;
   }
   set charisma(newCharm) {
     this._charisma = newCharm + this._charisma;
   }
-  set determination(newDetermination) {
-    this._determination = newDetermination + this._determination;
+  // get and set for feeling
+  get feeling() {
+    return this._feeling;
+  }
+  set feeling(newfeeling) {
+    this._feeling = newfeeling + this._feeling;
+  }
+  // get and set hobbies
+  get hobbies() {
+    return this._hobbies;
   }
   set hobbies(newHobby) {
     this._hobbies.push(newHobby);
   }
+  // get and set location
+  get location() {
+    return this._location;
+  }
   set location(newLocation) {
     this._location = newLocation;
+  }
+  // get and set employmentHistory
+  get employmentHistory() {
+    return this._employmentHistory;
   }
   set employmentHistory(newJob) {
     this._employmentHistory.push(newJob);
   }
+  //methods
   applyForJobs() {
     this._numberOfApplications++;
+  }
+  depression(newFeeling) {
+    this._feeling = this._feeling - newFeeling;
+  }
+  excited(newFeeling) {
+    this._feeling = this._feeling + newFeeling;
+  }
+  deposit(amount) {
+    this._money = this._money + amount;
+  }
+  withdrawal(amount) {
+    this._money = this._money + amount;
   }
 }
