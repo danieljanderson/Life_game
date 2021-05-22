df7860b796473ae475de03a2ec3ab898cc79719d
danieljanderson <danieljanderson0616@gmail.com>
2021-05-22 17:27:49 -0400

    added labels

--- src/PersonComponent.js
+++ src/PersonComponent.js
@@ -7,40 +7,40 @@ class PersonComponent extends Component{
         return(
             <div className = "PersonComponent">
                 <div className="PersonComponent-name">
-                    <h1>{this.props.name}</h1>
+                    <h1>Name: {this.props.name}</h1>
                 </div>
                 <div className="PersonComponent-birthdate">
-                    <p>{this.props.birthdate}</p>
+                    <p>Birthday: {this.props.birthdate}</p>
                 </div>
                 <div className="PersonComponent-gender">
-                    <p>{this.props.gender}</p>
+                    <p>Gender: {this.props.gender}</p>
                 </div>
                 <div className="PersonComponent-money">
-                    <p>{this.props.money}</p>
+                    <p>Money: {this.props.money}</p>
                 </div>
                 <div className="PersonComponent-charisma">
-                    <p>{this.props.charisma}</p>
+                    <p>Charisma: {this.props.charisma}</p>
                 </div>
                 <div className="PersonComponent-feeling">
-                    <p>{this.props.feeling}</p>
+                    <p>Feeling: {this.props.feeling}</p>
                 </div>
                 <div className="PersonComponent-intelligence">
-                    <p>{this.props.intelligence}</p>
+                    <p>Intelligence: {this.props.intelligence}</p>
                 </div>
                 <div className="PersonComponent-hobbies">
-                    <p>{this.props.hobbies}</p>
+                    <p>Hobbies: {this.props.hobbies}</p>
                 </div>
                 <div className="PersonComponent-location">
-                    <p>{this.props.location}</p>
+                    <p>Location: {this.props.location}</p>
                 </div>
                 <div className="PersonComponent-currentJob">
-                    <p>{this.props.currentJob}</p>
+                    <p>Current Job: {this.props.currentJob}</p>
                 </div>
                 <div className="PersonComponent-employmentHistory">
-                    <p>{this.props.employmentHistory}</p>
+                    <p>Employment History: {this.props.employmentHistory}</p>
                 </div>
                 <div className="PersonComponent-car">
-                    <p>{this.props.car}</p>
+                    <p>Owns a Car: {this.props.car}</p>
                 </div>
                 
             </div>
