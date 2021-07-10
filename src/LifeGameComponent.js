c4cb4ac3ba6faaed385930edb112b1bb679daf4c
danieljanderson <danieljanderson0616@gmail.com>
2021-07-10 14:39:04 -0400

    refectored to make person component dynamic

--- src/LifeGameComponent.js
+++ src/LifeGameComponent.js
@@ -5,7 +5,7 @@ import ChoiceComponent from './ChoiceComponent'
 
 class LifeGameComponent extends Component{
     static defaultProps ={
-        name:"Daniel",
+        name:"Charisse",
         birthdate: "6/12/1990",
         gender: "Male",
         money:1000,
@@ -18,23 +18,27 @@ class LifeGameComponent extends Component{
         employmentHistory:["State Farm","AutoZone"],
         car:true
     }
+    constructor(props){
+        super(props)
+        this.state={}
+    }
 
     render(){
         return(
         <div className="LifeGameComponent">
             <PersonComponent 
-                name="Daniel"
-                birthdate="06/12/1990"
-                gender="Male"
-                money={1000}
-                charisma={30}
-                feeling={50}
-                intelligence={100}
-                hobbies={["Golf","Coding","Board Games"]}
-                location="Cleveland Ohio"
-                currentJob="unemployed"
-                employmentHistory={["State Farm","AutoZone"]}
-                car="yes"
+                name={this.props.name}
+                birthdate={this.props.birthdate}
+                gender={this.props.gender}
+                money={this.props.money}
+                charisma={this.props.charisma}
+                feeling={this.props.feeling}
+                intelligence={this.props.intelligence}
+                hobbies={this.props.hobbies}
+                location={this.props.location}
+                currentJob={this.props.currentJob}
+                employmentHistory={this.props.currentJob}
+                car={this.props.car ? "Yes": "No"}
             />
             <ChoiceComponent></ChoiceComponent>
             
