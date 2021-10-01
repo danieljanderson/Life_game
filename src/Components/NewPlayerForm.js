a392ea686c4372f02019278acae250a653628458
Daniel Anderson <danieljanderson0616@gmail.com>
2021-10-01 17:10:37 -0400

    added all fields

--- src/Components/NewPlayerForm.js
+++ src/Components/NewPlayerForm.js
@@ -13,6 +13,12 @@ import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
 import { DatePicker } from '@mui/lab';
 import MenuItem from '@mui/material/MenuItem';
+import { Formik, Field, Form, ErrorMessage, FieldArray } from 'formik';
+import WcIcon from '@mui/icons-material/Wc';
+import IconButton from '@mui/material/IconButton';
+import LocationCityIcon from '@mui/icons-material/LocationCity';
+import ControlPointIcon from '@mui/icons-material/ControlPoint';
+import SportsTennisIcon from '@mui/icons-material/SportsTennis';
 
 function NewPlayerForm() {
   const [value, setValue] = React.useState(null);
@@ -60,19 +66,58 @@ function NewPlayerForm() {
           {/* end of birthday date picker */}
           {/* Start of Gender picker */}
           <Grid item md={3}>
-            <TextField
-              label="Gender"
-              select
-              id="Gender-Select"
-              value={gender}
-              sx={{ width: '100%' }}
-              onChange={handleChange}
-            >
-              <MenuItem value={'Male'}>Male</MenuItem>
-              <MenuItem value={'Female'}>Female</MenuItem>
-            </TextField>
+            <Box>
+              <TextField
+                label="Gender"
+                select
+                id="Gender-Select"
+                value={gender}
+                sx={{ width: '100%' }}
+                onChange={handleChange}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <WcIcon></WcIcon>
+                    </InputAdornment>
+                  ),
+                }}
+              >
+                <MenuItem value={'Male'}>Male</MenuItem>
+                <MenuItem value={'Female'}>Female</MenuItem>
+              </TextField>
+            </Box>
           </Grid>
           {/* End of Gender Picker */}
+          {/* Start of Location */}
+
+          <Grid item md={12}>
+            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+              <LocationCityIcon />
+              <TextField id="Location" label="Location" variant="standard" />
+            </Box>
+          </Grid>
+          {/* End of Location */}
+          {/* Strart of Hobbies Input */}
+          <Grid item md={5}>
+            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+              <TextField
+                id="Hobbies"
+                label="Please enter a Hobby"
+                variant="standard"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SportsTennisIcon></SportsTennisIcon>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <IconButton aria-label="Add an Hobby">
+                <ControlPointIcon></ControlPointIcon>
+              </IconButton>
+            </Box>
+          </Grid>
+          {/* End of Hobbies Input */}
         </Grid>
       </FormControl>
     </Box>
