1a0bc61d990f3e87efdd6eea65a0b0a1cc82013f
Daniel Anderson <danieljanderson0616@gmail.com>
2021-10-04 16:10:45 -0400

    small laptops done

--- src/Components/NewPlayerForm.js
+++ src/Components/NewPlayerForm.js
@@ -49,7 +49,7 @@ function NewPlayerForm() {
             direction="row"
           >
             {/* Start of First Name input*/}
-            <Grid item xs={12} sm={6} md={6}>
+            <Grid item xs={12} sm={6} md={3}>
               <Box>
                 <TextField
                   id="First-Name"
@@ -68,8 +68,8 @@ function NewPlayerForm() {
             {/* End of First Name input */}
 
             {/* Start of Last Name input */}
-            <Grid item xs={12} sm={6} md>
-              <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+            <Grid item xs={12} sm={6} md={3}>
+              <Box>
                 <TextField
                   id="Last-Name"
                   label="Last Name"
@@ -86,7 +86,7 @@ function NewPlayerForm() {
             </Grid>
             {/* End of Last Name input */}
             {/* This is for the start of the birthday date picker */}
-            <Grid item xs={6} sm={3} md>
+            <Grid item xs={6} sm={3} md={5}>
               <DatePicker
                 label="Birthday"
                 value={value}
@@ -98,7 +98,7 @@ function NewPlayerForm() {
             </Grid>
             {/* end of birthday date picker */}
             {/* Start of Gender picker */}
-            <Grid item xs={6} sm={2} md>
+            <Grid item xs={6} sm={2} md={3}>
               <Box>
                 <TextField
                   label="Gender"
@@ -123,7 +123,7 @@ function NewPlayerForm() {
             {/* End of Gender Picker */}
             {/* Start of Location */}
 
-            <Grid item xs={12} sm={5} md>
+            <Grid item xs={12} sm={5} md={5}>
               <Box>
                 <TextField
                   id="Location"
@@ -141,7 +141,7 @@ function NewPlayerForm() {
             </Grid>
             {/* End of Location */}
             {/* Strart of Hobbies Input */}
-            <Grid item xs={12} md>
+            <Grid item xs={12} md={12}>
               <Box>
                 <TextField
                   id="Hobbies"
